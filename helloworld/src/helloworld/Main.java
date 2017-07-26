a6d8a306e40183d34389f0b2dc998c1f91088577
Bart Simpson <BartSimpson@gmail.com>
2017-07-26 11:18:54 -0400

    mr jeff moved hello world

---
+++ helloworld/src/helloworld/Main.java
@@ -0,0 +1,15 @@
+package helloworld;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//hey I want to build a 1973 dodge charger with nos
+		 System.out.println("Hello World. how are you. good you. yes.");
+		 System.out.println("sssssssssssspppppppppaaaaaaaazzzzzzzzzzzz aaattttttaaaaccccckkkkk mmmooooooddddddeeeeeee (%##");
+	}
+//how are you 
+//good you
+//good
+}
+      
