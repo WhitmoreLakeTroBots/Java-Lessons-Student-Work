d314ad9dfa2629ea87cc39e29549962e8cbd92c1
Bart Simpson <BartSimpson@gmail.com>
2017-07-26 11:14:12 -0400

    mr jeff moved hello world to here

---
+++ helloworld/src/helloworld/Main.java
@@ -0,0 +1,21 @@
+ package helloworld;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+/*how are you doing */
+		// i hate you
+		// die in a hole
+		// die now
+		/* you die in a hole */
+		// you will die tonight
+		System.out.println("hello world");
+			// die now
+		System.out.println("ethan poole");
+		System.out.println("husky power");
+		System.out.println("death");
+		System.out.println("dogs");
+	}
+
+}
