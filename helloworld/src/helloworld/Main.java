380b70a882555c15e4c3f4531540f7d50bee3c53
WLRobotics <wlhsroboticsteam@gmail.com>
2017-07-26 11:30:41 -0400

    mr. jeff moved hello world.

---
+++ helloworld/src/helloworld/Main.java
@@ -0,0 +1,17 @@
+package helloworld;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//random comments
+		
+System.out.println("i am a dinosour");
+System.out.println("i eat plants");
+System.out.println("and other dinosours");
+System.out.println("i walk on ground");
+System.out.println("and i can swim!");
+System.out.println(" wait? can i swim?");
+	}
+
+}
