18aa427c1bd1e197f8324afc0ffcf4bf80ebab28
WL Robotics <WLHSRoboticsTeam@gmail.com>
2017-07-26 11:27:31 -0400

    mr. jeff moved helloworld

---
+++ helloworld/src/helloworld/Main.java
@@ -0,0 +1,13 @@
+package helloworld;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      // hi how are you doing
+		System.out.println("madea boo"); 
+		System.out.println("coming after you");
+		System.out.println("ha ha ha ha ha ha ha ");
+		}
+
+}
