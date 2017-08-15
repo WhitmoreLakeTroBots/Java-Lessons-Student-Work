ebf352d519e3ec9706a5d96474d2e3fffa7a5237
WL Robotics <wlhsroboticsteam@gmail.com>
2017-08-15 19:49:41 -0400

    jas first class commit

---
+++ helloworld/src/helloworld/Main.java
@@ -0,0 +1,16 @@
+package helloworld;
+
+public class Main {
+
+	public static void main (String[] args){
+		// 		some stuff here....
+		
+		/* 
+		 * some more stuff 
+		 * */
+		
+		System.out.println("Hello Mr. Jeff");
+		System.out.println("its hot in here. !");
+	}
+	
+}
